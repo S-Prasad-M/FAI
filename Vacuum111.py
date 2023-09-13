7977704134c18164d33cba48b2d4969c567745b0
Shree Prasad M <sprasad2804@gmail.com>
2023-09-13 08:04:02 +0000

    ...

--- Vacuum111.py
+++ Vacuum111.py
@@ -1,6 +1,48 @@
 A = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
 ]
 
+
+def print_F(floor, row, col):
+    """A function to print the GRID , (row, col) represent the current vacuum cleaner position"""
+    print("The Floor matrix is as below:")
+    for r in range(len(floor)):
+        for c in range(len(floor[r])):
+            if r == row and c == col:
+                print(f" >{floor[r][c]}< ", end='')
+            else:
+                print(f"  {floor[r][c]}  ", end='')
+        print(end='\n')
+    print(end='\n')
+
+
+def clean(floor):
+    i, j, row, col = 0, 0, len(floor), len(floor[0])
+    for i in range(row):
+        if (i % 2 == 0):
+            for j in range(col):
+                if (floor[i][j] == 1):
+                    print_F(floor, i, j)
+                    floor[i][j] = 0
+                print_F(floor, i, j)
+        else:
+            for j in range(col-1, -1, -1):
+                if (floor[i][j] == 1):
+                    print_F(floor, i, j)
+                    floor[i][j] = 0
+                print_F(floor, i, j)
+
+
+def print_F(floor, row, col):
+    """A function to print the GRID , (row, col) represent the current vacuum cleaner position"""
+    print("The Floor matrix is as below:")
+    for r in range(len(floor)):
+        for c in range(len(floor[r])):
+            if r == row and c == col:
+                print(f" >{floor[r][c]}< ", end='')
+            else:
+                print(f"  {floor[r][c]}  ", end='')
+        print(end='\n')
+    print(end='\n')
