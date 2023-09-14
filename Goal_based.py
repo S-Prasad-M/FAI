b48bd43120b8ef47eadfd520f953ad71c5ee3c0c
Shree Prasad M <sprasad2804@gmail.com>
2023-09-15 00:29:15 +0530

    Update Goal_based.py
    ....

--- Goal_based.py
+++ Goal_based.py
@@ -1,31 +1,25 @@
-import random
-env=[[0,0,0],
-    [0,"A",0],
-    [0,0,0]]
-
-row=int(input("enter row for dirt (0-2) : "))
-coloumn=int(input("enter coloumn for dirt (0-2) : "))
-env[row][coloumn]=1
-for i in env:
-    print(i)
-print("dirt added",end = '\n')
-movements=[-1,0,1]; curr=[1,1]; d=0; cp=[]; cost=0
-while env[curr[0]][curr[1]]!=2:
-    ch1=random.choice(movements); ch2=random.choice(movements)
-    if (curr[0]+ch1<len(env) and curr[0]+ch1>-1) and (curr[1]+ch2<len(env) and curr[1]+ch2>-1):
-        if (env[curr[0]+ch1][curr[1]+ch2])==1:
-            env[curr[0]+ch1][curr[1]+ch2]="A"; env[curr[0]][curr[1]]=0
-            for i in env:
-                print(i)
-            print()
-            print("dirt cleaned")
-            cost+=1
-            break
-        if (env[curr[0]+ch1][curr[1]+ch2])==0 and [curr[0]+ch1,curr[1]+ch2]:
-            env[curr[0]+ch1][curr[1]+ch2]="A"; env[curr[0]][curr[1]]= 0
-            for i in env:
-                print(i)
-            print()
-            curr[0]+=ch1; curr[1]+=ch2
-            cost+=1
-print("Path cost : ",cost)
+A = [[0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0,0,0]
+    ]
+def distance(point1, point2):
+    x1 = point1[0]; x2 = point2[0]
+    y1 = point1[1]; y2 = point2[0]
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2)**0.5
+point1 = [2,0]
+point2 = [2,4]
+center = [(point1[0] + point2[0]) // 2, (point1[1] + point2[1]) // 2]
+radius = distance(point1, center)
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        if distance([i, j], center) <= radius:
+            A[i][j] = 1
+for row in A:
+    print(row)
